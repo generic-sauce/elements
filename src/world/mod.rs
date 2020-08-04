5f6b3a635e06bc2753e30de190cb76d95f445fd5
memoryleak47 <memoryleak47@protonmail.ch>
2020-08-04 17:14:33 +0200

    added tilemap

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,21 +1,25 @@
 mod player;
+mod tilemap;
 
 use sfml::system::Vector2f;
 use sfml::graphics::RenderWindow;
 
 use crate::input::{Input, KeyboardInput};
 use crate::world::player::Player;
+use crate::world::tilemap::TileMap;
 use crate::texture_state::TextureState;
 
 pub struct World {
 	pub players: [Player; 2],
-    pub input: Box<dyn Input>,
+	pub tilemap: TileMap,
+	pub input: Box<dyn Input>, // TODO move this to App
 }
 
 impl World {
 	pub fn new() -> World {
 		World {
 			players: [Player::new(Vector2f::new(0.0, 0.0)), Player::new(Vector2f::new(20.0, 0.0))],
+			tilemap: TileMap::new("res/todo-add-path"),
 			input: Box::new(KeyboardInput::new()),
 		}
 	}
