101e36edce02794f8cae6af4b21d262c5bcde041
ichinii <sven.salt@yahoo.de>
2020-08-04 22:54:20 +0000

    tile colors

--- src/world/tilemap/draw.rs
+++ src/world/tilemap/draw.rs
@@ -7,8 +7,8 @@ impl TileMap {
             let position = Vec2f::new((index % self.size.x) as f32, (index / self.size.x) as f32) + Vec2f::new(0.5, 0.5);
 
             let color = match tile {
-                Tile::Void => Color::rgb(32, 32, 32),
-                Tile::Ground => Color::rgb(64, 64, 64),
+                Tile::Void => Color::rgb(115, 158, 65),
+                Tile::Ground => Color::rgb(51, 26, 26),
             };
             context.draw_sprite(position, Vec2f::new(0.5, 0.5), color, None);
         }
