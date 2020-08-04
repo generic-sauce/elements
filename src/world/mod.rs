a6fb91eb73976491d140bec895d5227910cdf875
Bluemi <bruno.schilling@protonmail.ch>
2020-08-04 16:05:41 +0200

    local player direction adjustment

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -17,14 +17,34 @@ impl World {
 	}
 
 	pub fn tick(&mut self) {
+		self.handle_local_player();
 		for p in self.players.iter_mut() {
 			p.tick();
 		}
+		// println!("local player {} {}", self.players[0].position.x, self.players[0].position.y);
 	}
 
 	pub fn render(&mut self, _w: &mut RenderWindow) {
 		for _p in self.players.iter_mut() {
 			// p.render(w); // TODO
 		}
+	}
+
+	fn handle_local_player(&mut self) {
+		let mut direction = Vector2f::new(0.0, 0.0);
+		if sfml::window::Key::W.is_pressed() {
+			direction.y += 1.0;
+		}
+		if sfml::window::Key::S.is_pressed() {
+			direction.y -= 1.0;
+		}
+
+		if sfml::window::Key::D.is_pressed() {
+			direction.x += 1.0;
+		}
+		if sfml::window::Key::A.is_pressed() {
+			direction.x -= 1.0;
+		}
+        self.players[0].direction = direction;
 	}
 }
