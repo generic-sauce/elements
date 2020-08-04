32a8741647435da91e814d27292ef4978c35f065
memoryleak47 <memoryleak47@protonmail.ch>
2020-08-04 19:00:20 +0200

    finished tilemap parsing

--- src/world/tilemap.rs
+++ src/world/tilemap.rs
@@ -22,7 +22,9 @@ impl TileMap {
 		for y in 0..s.y {
 			for x in 0..s.x {
 				let tile = match image.pixel_at(x as u32, y as u32) {
-					Color { r, g, b, a } => Tile::Void, // TODO do anything else than void!
+					Color { r: 255, g: 255, b: 255, a: 255 } => Tile::Void,
+					Color { r: 0, g: 0, b: 0, a: 255 } => Tile::Ground,
+					c => panic!("tile color out of range! {:?}", c),
 				};
 				tiles.push(tile);
 			}
