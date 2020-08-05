b89aade84a621ab0c471bf7a3633ea1966120dbe
memoryleak47 <memoryleak47@protonmail.ch>
2020-08-05 23:18:42 +0200

    bugfix in player collision

--- src/world/player/mod.rs
+++ src/world/player/mod.rs
@@ -54,10 +54,10 @@ impl Player {
 
 fn is_colliding(left_bot: Vec2i, t: &TileMap) -> bool {
 	let x_min = left_bot.x / TILESIZE;
-	let x_max = (left_bot.x + PLAYER_SIZE.x - 1) / TILESIZE + 1;
+	let x_max = (left_bot.x + PLAYER_SIZE.x - 1) / TILESIZE; // recall that PLAYER_SIZE = 1, means that x_min = x_max
 
 	let y_min = left_bot.y / TILESIZE;
-	let y_max = (left_bot.y + PLAYER_SIZE.y - 1) / TILESIZE + 1;
+	let y_max = (left_bot.y + PLAYER_SIZE.y - 1) / TILESIZE;
 
 	// TODO write this using .any()?
 	for x in x_min..=x_max {
