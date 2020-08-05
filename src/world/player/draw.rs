d62fca0fce82fc51038a4c539e3477930111e2b4
memoryleak47 <memoryleak47@protonmail.ch>
2020-08-06 00:11:36 +0200

    did correct player-scaling

--- src/world/player/draw.rs
+++ src/world/player/draw.rs
@@ -1,11 +1,28 @@
 use crate::prelude::*;
 
-impl Player {
-    pub fn draw(&mut self, context: &Context) {
-		// TODO calculate render-size of player!
-		let center = (self.left_bot.to_f() + (PLAYER_SIZE.to_f() / 2.0)) / TILESIZE as f32;
-		let radius = PLAYER_SIZE.to_f() / TILESIZE as f32 / 2.;
+const IMG_SIZE: f32 = 64.0;
+const IMG_PLAYER_WIDTH: f32 = 22.0;
+const IMG_PLAYER_HEIGHT: f32 = 55.0;
 
-        context.draw_sprite(center, radius, Color::WHITE, Some(TextureId::PlayerIdle1));
+const FACTOR_X: f32 = IMG_SIZE / IMG_PLAYER_WIDTH;
+const FACTOR_Y: f32 = IMG_SIZE / IMG_PLAYER_HEIGHT;
+
+impl Player {
+	// the texture rect has been obtained by reading the .png file
+
+	fn texture_center(&self) -> Vec2f {
+		Vec2f::new(
+			(self.left_bot.x + PLAYER_SIZE.x / 2) as f32 / TILESIZE as f32,
+			(self.left_bot.y as f32 / TILESIZE as f32)
+				+ (PLAYER_SIZE.y as f32 / 2.0 * FACTOR_Y) / TILESIZE as f32,
+		)
+	}
+
+	fn texture_radius(&self) -> Vec2f {
+		(PLAYER_SIZE.to_f() / TILESIZE as f32) / 2.0 * Vec2f::new(FACTOR_X, FACTOR_Y)
+	}
+
+    pub fn draw(&mut self, context: &Context) {
+        context.draw_sprite(self.texture_center(), self.texture_radius(), Color::WHITE, Some(TextureId::PlayerIdle1));
     }
 }
