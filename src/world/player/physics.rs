0b54040fbb32b8707c5da89aa4ce143f304c6e84
memoryleak47 <memoryleak47@protonmail.ch>
2020-08-06 03:01:34 +0200

    dist -> route

--- src/world/player/physics.rs
+++ src/world/player/physics.rs
@@ -5,21 +5,20 @@ impl Player {
 		let mut remaining_vel = self.velocity;
 
 		while remaining_vel != 0.into() {
-			let xdist = dist(remaining_vel.x, self.left_bot.x, PLAYER_SIZE.x);
-			let ydist = dist(remaining_vel.y, self.left_bot.y, PLAYER_SIZE.y);
+			let xroute = route(remaining_vel.x, self.left_bot.x, PLAYER_SIZE.x);
+			let yroute = route(remaining_vel.y, self.left_bot.y, PLAYER_SIZE.y);
 
-			assert!(xdist > 0);
-			assert!(ydist > 0);
+			assert!(xroute != 0);
+			assert!(yroute != 0);
 
-			if xdist > remaining_vel.x.abs() && ydist > remaining_vel.y.abs() { // if no more collisions can happen!
+			if xroute.abs() > remaining_vel.x.abs() && yroute.abs() > remaining_vel.y.abs() { // if no more collisions can happen!
 				self.left_bot += remaining_vel;
 				break;
-			} else if (xdist * self.velocity.y).abs() < (ydist * self.velocity.x).abs() { //    <->    xdist / self.velocity.x < ydist / self.velocity.y    <->    xtime < ytime
+			} else if (xroute * self.velocity.y).abs() < (yroute * self.velocity.x).abs() { //    <->    xdist / self.velocity.x < ydist / self.velocity.y    <->    xtime < ytime
 				assert!(false);
 
-				let change_x = xdist * remaining_vel.x.signum();
-				let change_y = xdist * remaining_vel.y / remaining_vel.x.abs();
-				let change = Vec2i::new(change_x, change_y);
+				let ychange = xroute.abs() * remaining_vel.y / remaining_vel.x.abs();
+				let change = Vec2i::new(xroute, ychange);
 
 				remaining_vel -= change;
 				self.left_bot += change;
@@ -28,9 +27,8 @@ impl Player {
 				// remaining_vel.x = 0;
 				// self.velocity.x = 0;
 			} else {
-				let change_x = ydist * remaining_vel.x / remaining_vel.y.abs();
-				let change_y = ydist * remaining_vel.y.signum();
-				let change = Vec2i::new(change_x, change_y);
+				let xchange = yroute.abs() * remaining_vel.x / remaining_vel.y.abs();
+				let change = Vec2i::new(xchange, yroute);
 
 				assert!(change != 0.into());
 
@@ -60,9 +58,9 @@ fn is_colliding(left_bot: Vec2i, t: &TileMap) -> bool {
 	false
 }
 
-fn dist(velocity: i32, min: i32, size: i32) -> i32 {
+fn route(velocity: i32, min: i32, size: i32) -> i32 {
 	if velocity < 0 {
-		(min % TILESIZE) + 1
+		-(min % TILESIZE) - 1
 	} else {
 		let max = min + size - 1;
 		(max / TILESIZE) + 1
