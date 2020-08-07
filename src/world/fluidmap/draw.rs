d1e8f32b954ac17d38c563c5087f9a66fe7be384
ichinii <sven.salt@yahoo.de>
2020-08-07 20:06:26 +0000

    optimize bounds check

--- src/world/fluidmap/draw.rs
+++ src/world/fluidmap/draw.rs
@@ -13,10 +13,10 @@ impl FluidMap {
 			let local_position = Vec2u8::new((fluid.position.x % TILESIZE) as u8, (fluid.position.y % TILESIZE) as u8);
 
 			let cell_index = 4 * (cell_id.x + cell_id.y * context.tilemap_size.x as i32) as usize;
-			pixels[cell_index+0] = local_position.x as u8;
-			pixels[cell_index+1] = local_position.y as u8;
-			pixels[cell_index+2] = 0;
 			pixels[cell_index+3] = 255;
+			pixels[cell_index+2] = 0;
+			pixels[cell_index+1] = local_position.y as u8;
+			pixels[cell_index+0] = local_position.x as u8;
 		}
 
 		let image = Image::create_from_pixels(context.tilemap_size.x as u32, context.tilemap_size.y as u32, &pixels).unwrap();
