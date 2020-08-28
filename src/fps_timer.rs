02fab7a013c7e22dc0f7cb49606098626a327e6a
memoryleak47 <memoryleak47@protonmail.ch>
2020-08-28 15:19:41 +0200

    improved fps_timer

--- src/fps_timer.rs
+++ src/fps_timer.rs
@@ -5,10 +5,10 @@ macro_rules! time {
 		let r = $action;
 		let after = std::time::SystemTime::now();
 
-		let time_it_took_ms = after.duration_since(before).unwrap().as_millis();
-		let frame_time_ms: u128 = 1000/60;
-		let ratio = 100 * time_it_took_ms / frame_time_ms;
-		println!("{} took {} ms ({}%)", std::stringify!($action), time_it_took_ms, ratio);
+		let time_it_took_micros = after.duration_since(before).unwrap().as_micros();
+		let frame_time_micros: u128 = 1000_000/60;
+		let ratio = 100 * time_it_took_micros / frame_time_micros;
+		println!("{} took {} ms ({}%)", std::stringify!($action), time_it_took_micros as f64 / 1000.0, ratio);
 		r
 	}}
 }
@@ -20,10 +20,10 @@ macro_rules! time_named {
 		let r = $action;
 		let after = std::time::SystemTime::now();
 
-		let time_it_took_ms = after.duration_since(before).unwrap().as_millis();
-		let frame_time_ms: u128 = 1000/60;
-		let ratio = 100 * time_it_took_ms / frame_time_ms;
-		println!("{:?} took {} ms ({}%)", $name, time_it_took_ms, ratio);
+		let time_it_took_micros = after.duration_since(before).unwrap().as_micros();
+		let frame_time_micros: u128 = 1000_000/60;
+		let ratio = 100 * time_it_took_micros / frame_time_micros;
+		println!("{} took {} ms ({}%)", std::stringify!($action), time_it_took_micros as f64 / 1000.0, ratio);
 		r
 	}}
 }
