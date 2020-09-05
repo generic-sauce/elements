a9998399e906903a2c957cb260ef5d2175fa44e0
Bluemi <bruno.schilling@protonmail.ch>
2020-09-05 13:13:17 +0200

    fix world update

--- src/world/update.rs
+++ src/world/update.rs
@@ -7,6 +7,7 @@ pub struct WorldUpdate {
 	pub fluidmap_update: FluidMapUpdate,
 	pub frame_id: u32,
 	pub kills: [u32; 2],
+	pub restart_state: RestartState,
 }
 
 impl World {
@@ -17,6 +18,7 @@ impl World {
 			fluidmap_update: self.fluidmap.update(),
 			frame_id: self.frame_id,
 			kills: self.kills,
+			restart_state: self.restart_state,
 		}
 	}
 
@@ -27,8 +29,9 @@ impl World {
 		self.fluidmap.apply_update(u.fluidmap_update);
 		self.frame_id = u.frame_id;
 		self.kills = u.kills;
+		self.restart_state = u.restart_state;
 
 		// This exists to generate a compiler error whenever a field will be added to World.
-		let Self { players: _, tilemap: _, fluidmap: _, frame_id: _, kills: _ } = self;
+		let Self { players: _, tilemap: _, fluidmap: _, frame_id: _, kills: _, restart_state: _ } = self;
 	}
 }
