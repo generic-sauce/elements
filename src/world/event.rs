2d85807b303be6a7fd5fc9d869ef65155a063983
memoryleak47 <memoryleak47@protonmail.ch>
2020-09-06 19:16:19 +0200

    ...

--- src/world/event.rs
+++ src/world/event.rs
@@ -8,26 +8,33 @@ impl EventHandler for () {
 	fn damage_inflicted(&mut self, _damage: i32, _player: usize) {}
 }
 
-pub struct AppEventHandler {
-	pub tilemap_changed: bool,
-	pub damages: [i32; 2],
-}
+#[cfg(feature = "client")]
+mod client {
+	use super::EventHandler;
 
-impl AppEventHandler {
-	pub fn new() -> AppEventHandler {
-		AppEventHandler {
-			tilemap_changed: false,
-			damages: [0; 2],
+	pub struct AppEventHandler {
+		pub tilemap_changed: bool,
+		pub damages: [i32; 2],
+	}
+
+	impl AppEventHandler {
+		pub fn new() -> AppEventHandler {
+			AppEventHandler {
+				tilemap_changed: false,
+				damages: [0; 2],
+			}
+		}
+	}
+
+	impl EventHandler for AppEventHandler {
+		fn tilemap_changed(&mut self) {
+			self.tilemap_changed = true;
+		}
+
+		fn damage_inflicted(&mut self, damage: i32, player: usize) {
+			self.damages[player] += damage;
 		}
 	}
 }
-
-impl EventHandler for AppEventHandler {
-	fn tilemap_changed(&mut self) {
-		self.tilemap_changed = true;
-	}
-
-	fn damage_inflicted(&mut self, damage: i32, player: usize) {
-		self.damages[player] += damage;
-	}
-}
+#[cfg(feature = "client")]
+pub use client::*;
