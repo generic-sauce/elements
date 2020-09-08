3acfc038877269872c0c5f9922193b98142e09a8
Bluemi <bruno.schilling@protonmail.ch>
2020-09-08 16:23:59 +0200

    expanded hud

--- src/draw/hud.rs
+++ src/draw/hud.rs
@@ -1,8 +1,8 @@
 use crate::prelude::*;
 
 pub(in super) fn draw(pl: &Player, target: &impl RenderTarget, context: &DrawContext) {
-	let mut size = GameVec::new(PLAYER_SIZE.x / 2, TILESIZE / 3);
-	let offset = GameVec::new(0, PLAYER_SIZE.y + TILESIZE);
+	let mut size = GameVec::new(PLAYER_SIZE.x, TILESIZE / 3);
+	let offset = GameVec::new(-PLAYER_SIZE.x, PLAYER_SIZE.y + TILESIZE);
 	let left_bot = pl.left_bot + offset;
 	context.draw_rect(target, left_bot, size, Color::BLACK, Origin::LeftBottom);
 	size.x = (size.x as f32 * (pl.health as f32 / MAX_HEALTH as f32)) as i32;
