e4567ef810ede83348b1c17e4c1eb3745797b791
memoryleak47 <memoryleak47@protonmail.ch>
2020-09-16 20:19:22 +0200

    ~fluids physics [WIP]

--- src/world/fluidmap/physics.rs
+++ src/world/fluidmap/physics.rs
@@ -6,38 +6,11 @@ enum XY {
 }
 
 enum Collision {
-	Fluid { me: usize, other: usize, },
+	Fluid { idx: usize },
 	Tile(XY),
 }
 
-/*
-fn find_next_collision(fm: &FluidMap, pos: GameVec, velocity: GameVec, remaining_vel: GameVec) -> Option<Collision> {
-	let fluid_iter = None.into_iter();
-
-	let tile_x_iter =
-		Some(Collision::Tile(XY::X))
-			.filter(|_| remaining_vel.x != 0)
-			.into_iter();
-
-	let tile_y_iter =
-		Some(Collision::Tile(XY::Y))
-			.filter(|_| remaining_vel.y != 0)
-			.into_iter();
-
-	fluid_iter.chain(tile_x_iter).chain(tile_y_iter)
-		.min_by_key(|c| c.change_until_touch.length_squared())
-		.filter(|c| c.change_until_touch.length_squared() <= remaining_vel.length_squared())
-}
- */
-
-// returns the change required to touch, but not collide the next tile
-fn route(velocity: i32, pos: i32) -> i32 {
-	if velocity < 0 {
-		-(pos % TILESIZE)
-	} else {
-		(TILESIZE-1) - (pos % TILESIZE)
-	}
-}
+fn reflect(x: i32) -> i32 { -x / 3 } // TODO add some randomness.
 
 impl FluidMap {
 	pub(in super) fn tick_physics(&mut self, t: &TileMap) {
@@ -57,6 +30,87 @@ impl FluidMap {
 		}
 	}
 
+	fn find_next_collision(&self, pos: GameVec, velocity: GameVec, remaining_vel: GameVec) -> Option<Collision> {
+		/*
+		let fluid_iter = None.into_iter();
+
+		let tile_x_iter =
+			Some(Collision::Tile(XY::X))
+				.filter(|_| remaining_vel.x != 0)
+				.into_iter();
+
+		let tile_y_iter =
+			Some(Collision::Tile(XY::Y))
+				.filter(|_| remaining_vel.y != 0)
+				.into_iter();
+
+		fluid_iter.chain(tile_x_iter).chain(tile_y_iter)
+			.min_by_key(|c| c.change_until_touch.length_squared())
+			.filter(|c| c.change_until_touch.length_squared() <= remaining_vel.length_squared())
+		 */
+		unimplemented!()
+	}
+
+	fn handle_collision(&mut self, f: &mut Fluid, c: Collision, remaining_vel: &mut GameVec, t: &TileMap) {
+		match c {
+			Collision::Tile(XY::X) => {
+				assert!(remaining_vel.x != 0);
+
+				let xroute = route(remaining_vel.x, f.position.x);
+				let yroute = route(remaining_vel.y, f.position.y);
+
+				let xroute_ex = xroute + remaining_vel.x.signum();
+				let yroute_ex = yroute + remaining_vel.y.signum();
+
+				let ychange = xroute.abs() * remaining_vel.y / remaining_vel.x.abs();
+				let change = GameVec::new(xroute, ychange);
+
+				let change_ex = change + (remaining_vel.x.signum(), 0);
+				if t.check_solid(f.position + change_ex) {
+					assert!(!t.check_solid(f.position + change));
+
+					*remaining_vel -= change;
+					f.position += change;
+
+					remaining_vel.x = 0;
+					f.velocity.x = reflect(f.velocity.x);
+				} else {
+					*remaining_vel -= change_ex;
+					f.position += change_ex;
+				}
+			},
+			Collision::Tile(XY::Y) => {
+				assert!(remaining_vel.y != 0);
+
+				let xroute = route(remaining_vel.x, f.position.x);
+				let yroute = route(remaining_vel.y, f.position.y);
+
+				let xroute_ex = xroute + remaining_vel.x.signum();
+				let yroute_ex = yroute + remaining_vel.y.signum();
+
+				let xchange = yroute.abs() * remaining_vel.x / remaining_vel.y.abs();
+				let change = GameVec::new(xchange, yroute);
+
+				let change_ex = change + (0, remaining_vel.y.signum());
+				if t.check_solid(f.position + change_ex) {
+					assert!(!t.check_solid(f.position + change));
+
+					*remaining_vel -= change;
+					f.position += change;
+
+					remaining_vel.y = 0;
+					f.velocity.y = reflect(f.velocity.y);
+				} else {
+					*remaining_vel -= change_ex;
+					f.position += change_ex;
+				}
+			},
+			Collision::Fluid { .. } => {
+				unimplemented!()
+			}
+		}
+	}
+
 	fn move_fluid(&mut self, f: &mut Fluid, t: &TileMap) {
 		if t.check_solid(f.position) {
 			#[cfg(debug_assertions)]
@@ -66,57 +120,19 @@ impl FluidMap {
 
 		let mut remaining_vel = f.velocity;
 
-		let reflect = |x: i32| -x / 3; // TODO add some randomness.
-
-		while remaining_vel != 0.into() {
-			let xroute = route(remaining_vel.x, f.position.x);
-			let yroute = route(remaining_vel.y, f.position.y);
-
-			let xroute_ex = xroute + remaining_vel.x.signum();
-			let yroute_ex = yroute + remaining_vel.y.signum();
-
-			if xroute.abs() >= remaining_vel.x.abs() && yroute.abs() >= remaining_vel.y.abs() { // if no more collisions can happen!
-				f.position += remaining_vel;
-				break;
-			} else if (remaining_vel.y == 0 && yroute_ex == 0) /* edge case */ || (xroute_ex * remaining_vel.y).abs() < (yroute_ex * remaining_vel.x).abs() { //    <->    xroute / self.velocity.x < yroute / self.velocity.y    <->    xtime < ytime
-				assert!(remaining_vel.x != 0);
-
-				let ychange = xroute.abs() * remaining_vel.y / remaining_vel.x.abs();
-				let change = GameVec::new(xroute, ychange);
-
-				let change_ex = change + (remaining_vel.x.signum(), 0);
-				if t.check_solid(f.position + change_ex) {
-					assert!(!t.check_solid(f.position + change));
-
-					remaining_vel -= change;
-					f.position += change;
-
-					remaining_vel.x = 0;
-					f.velocity.x = reflect(f.velocity.x);
-				} else {
-					remaining_vel -= change_ex;
-					f.position += change_ex;
-				}
-			} else {
-				assert!(remaining_vel.y != 0);
-
-				let xchange = yroute.abs() * remaining_vel.x / remaining_vel.y.abs();
-				let change = GameVec::new(xchange, yroute);
-
-				let change_ex = change + (0, remaining_vel.y.signum());
-				if t.check_solid(f.position + change_ex) {
-					assert!(!t.check_solid(f.position + change));
-
-					remaining_vel -= change;
-					f.position += change;
-
-					remaining_vel.y = 0;
-					f.velocity.y = reflect(f.velocity.y);
-				} else {
-					remaining_vel -= change_ex;
-					f.position += change_ex;
-				}
-			}
+		while let Some(coll) = self.find_next_collision(f.position, f.velocity, remaining_vel) {
+			self.handle_collision(f, coll, &mut remaining_vel, t);
 		}
+		f.position += remaining_vel;
 	}
 }
+
+// returns the change required to touch, but not collide the next tile
+fn route(velocity: i32, pos: i32) -> i32 {
+	if velocity < 0 {
+		-(pos % TILESIZE)
+	} else {
+		(TILESIZE-1) - (pos % TILESIZE)
+	}
+}
+
