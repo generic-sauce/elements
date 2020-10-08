acffbb74818afabe922c99c9d02120d1d168176a
Bluemi <bruno.schilling@protonmail.ch>
2020-10-08 17:07:20 +0200

    worked on menu

---
+++ src/menu/menu_elements.rs
@@ -0,0 +1,96 @@
+use crate::prelude::*;
+
+const BUTTON_TEXT_SIZE: f32 = 0.05;
+const EDIT_FIELD_BORDER_WIDTH: f32 = 0.004;
+
+pub struct MenuElement {
+	pub kind: MenuKind,
+	pub position: CanvasVec,
+	pub size: CanvasVec,
+	pub hovered: bool,
+	pub clicked: bool,
+}
+
+pub enum MenuKind {
+	Button {
+		text: &'static str,
+		runnable_change: RunnableChange,
+	},
+	EditField {
+		text: String,
+		selected: bool,
+	}
+}
+
+impl MenuElement {
+	pub fn new_button(position: CanvasVec, size: CanvasVec, text: &'static str, runnable_change: RunnableChange) -> MenuElement {
+		MenuElement {
+			kind: MenuKind::Button { runnable_change, text },
+			position,
+			size,
+			hovered: false,
+			clicked: false,
+		}
+	}
+
+	pub fn new_edit_field(position: CanvasVec, size: CanvasVec, text: &str) -> MenuElement {
+		MenuElement {
+			kind: MenuKind::EditField { text: String::from(text), selected: false },
+			position,
+			size,
+			hovered: false,
+			clicked: false,
+		}
+	}
+
+	pub fn is_colliding(&self, pos: &CanvasVec) -> bool {
+		pos.x >= self.position.x - self.size.x && pos.x <= self.position.x + self.size.x &&
+		pos.y >= self.position.y - self.size.y && pos.y <= self.position.y + self.size.y
+	}
+
+	pub fn draw(&self, target: &RenderWindow, context: &mut DrawContext, cursor_pos: &CanvasVec) {
+		let color = if self.clicked {
+			Color::rgb(47, 110, 140)
+		} else if self.is_colliding(cursor_pos) {
+			Color::rgb(32, 82, 120)
+		} else {
+			Color::rgb(21, 67, 109)
+		};
+		match &self.kind {
+			MenuKind::Button { text, runnable_change: _runnable_change } => { self.draw_button(target, context, text, color) },
+			MenuKind::EditField { text, selected } => { self.draw_edit_field(target, context, text, color, *selected) },
+		}
+	}
+
+	fn draw_button(&self, target: &RenderWindow, context: &mut DrawContext, text: &str, color: Color) {
+		context.draw_rect(target, self.position, self.size, color, Origin::Center);
+		context.draw_text(target, self.position - CanvasVec::new(text.len() as f32 * BUTTON_TEXT_SIZE / 5.5, 0.45 * BUTTON_TEXT_SIZE), BUTTON_TEXT_SIZE, &text, Origin::LeftBottom);
+	}
+
+	fn draw_edit_field(&self, target: &RenderWindow, context: &mut DrawContext, text: &str, color: Color, selected: bool) {
+		context.draw_rect(target, self.position, self.size, color, Origin::Center);
+		context.draw_rect(
+			target,
+			self.position,
+			self.size - CanvasVec::new(EDIT_FIELD_BORDER_WIDTH, EDIT_FIELD_BORDER_WIDTH),
+			Color::BLACK,
+			Origin::Center
+		);
+		if selected {
+			context.draw_rect(
+				target,
+				CanvasVec::new(self.position.x - self.size.x + 0.01, self.position.y),
+				CanvasVec::new(0.001, self.size.y * 0.7),
+				Color::WHITE,
+				Origin::Center
+			);
+		}
+		context.draw_text(
+			target,
+			self.position - CanvasVec::new(text.len() as f32 * BUTTON_TEXT_SIZE / 5.5, 0.45 * BUTTON_TEXT_SIZE),
+			BUTTON_TEXT_SIZE,
+			&text,
+			Origin::LeftBottom
+		);
+	}
+}
---
+++ src/menu/mod.rs
@@ -0,0 +1,135 @@
+mod menu_elements;
+
+pub use menu_elements::*;
+use crate::prelude::*;
+
+pub const DEFAULT_CURSOR_POSITION: CanvasVec = CanvasVec::new(0.5 * 16.0 / 9.0, 0.5);
+pub const ASPECT_RATIO: f32 = 16.0 / 9.0;
+
+pub struct Menu {
+	pub elements: Vec<MenuElement>,
+}
+
+pub struct MenuRunnable {
+	pub menu: Menu,
+	pub next_runnable_change: RunnableChange,
+}
+
+impl Menu {
+	pub fn main_menu() -> Menu {
+		Menu {
+			elements: vec!(
+				MenuElement::new_button(CanvasVec::new(0.3 * ASPECT_RATIO, 0.6), CanvasVec::new(0.15, 0.05), "Best of 9", RunnableChange::Game(9)),
+				MenuElement::new_button(CanvasVec::new(0.3 * ASPECT_RATIO, 0.4), CanvasVec::new(0.15, 0.05), "Best of 5", RunnableChange::Game(5)),
+				MenuElement::new_button(CanvasVec::new(0.7 * ASPECT_RATIO, 0.6), CanvasVec::new(0.15, 0.05), "Infinite Game", RunnableChange::Game(0)),
+				MenuElement::new_button(CanvasVec::new(0.7 * ASPECT_RATIO, 0.4), CanvasVec::new(0.15, 0.05), "Connect to Server", RunnableChange::Menu(MenuChoice::ConnectServer)),
+				MenuElement::new_button(CanvasVec::new(0.85 * ASPECT_RATIO, 0.15), CanvasVec::new(0.15, 0.05), "Quit", RunnableChange::Quit),
+			),
+		}
+	}
+
+	pub fn connect_server_menu() -> Menu {
+		Menu {
+			elements: vec!(
+				MenuElement::new_button(CanvasVec::new(0.5 * ASPECT_RATIO, 0.4), CanvasVec::new(0.15, 0.05), "Connect", RunnableChange::Game(5)),
+				MenuElement::new_button(CanvasVec::new(0.15 * ASPECT_RATIO, 0.15), CanvasVec::new(0.15, 0.05), "Back", RunnableChange::Menu(MenuChoice::Main)),
+				MenuElement::new_button(CanvasVec::new(0.85 * ASPECT_RATIO, 0.15), CanvasVec::new(0.15, 0.05), "Quit", RunnableChange::Quit),
+				MenuElement::new_edit_field(CanvasVec::new(0.5 * ASPECT_RATIO, 0.6), CanvasVec::new(0.15, 0.03), ""),
+			)
+		}
+	}
+
+	pub fn get_clicked_element(&mut self) -> Option<&mut MenuElement> {
+		self.elements.iter_mut().find(|e| e.clicked)
+	}
+
+	pub fn get_selected_element(&mut self) -> Option<&mut MenuElement> {
+		self.elements.iter_mut().find(|e| if let MenuKind::EditField { selected, .. } = e.kind { selected } else { false })
+	}
+}
+
+impl MenuRunnable {
+	pub fn new(menu_choice: MenuChoice) -> MenuRunnable {
+		reset_mouse_position();
+		let menu = match menu_choice {
+			MenuChoice::Main => Menu::main_menu(),
+			MenuChoice::ConnectServer => Menu::connect_server_menu(),
+		};
+		MenuRunnable {
+			menu,
+			next_runnable_change: RunnableChange::None,
+		}
+	}
+}
+
+impl Runnable for MenuRunnable {
+	fn tick(&mut self, app: &mut App) {
+		let mouse_update = get_mouse_position_update();
+		app.cursor_position += CanvasVec::new(mouse_update.x, -mouse_update.y) * 0.001;
+		app.cursor_position.y = app.cursor_position.y.max(0.0).min(1.0);
+		app.cursor_position.x = app.cursor_position.x.max(0.0).min(ASPECT_RATIO);
+
+		if sfml::window::mouse::Button::Left.is_pressed() {
+			for element in &mut self.menu.elements {
+				element.clicked = element.is_colliding(&app.cursor_position);
+			}
+			if let Some(elem) = self.menu.get_selected_element() {
+				if let MenuKind::EditField { selected, .. } = &mut elem.kind {
+					*selected = false;
+				}
+			}
+		} else {
+			if let Some(element) = self.menu.get_clicked_element() {
+				element.clicked = false;
+				match &mut element.kind {
+					MenuKind::Button { runnable_change, .. } => {
+						self.next_runnable_change = *runnable_change;
+					}
+					MenuKind::EditField { selected, .. } => {
+						*selected = true;
+					}
+				}
+			}
+		}
+	}
+
+	fn draw(&mut self, app: &mut App, timed_loop_info: &TimedLoopInfo) {
+		app.window.display();
+		app.window.clear(Color::BLACK);
+
+		let aspect_ratio = 16.0 / 9.0;
+		let (window_view, view, view_pixel_size) = get_views(app, aspect_ratio);
+
+		let mut game_texture_target = RenderTexture::new(view_pixel_size.x as u32, view_pixel_size.y as u32, false).unwrap();
+		game_texture_target.set_view(&view);
+		app.window.set_view(&window_view);
+
+		let window_size = app.window.size();
+		let window_size = WindowVec::new(window_size.x as f32, window_size.y as f32);
+
+		let mut context = DrawContext {
+			window_size,
+			texture_state: &app.texture_state,
+			shader_state: &mut app.shader_state,
+			font_state: &app.font_state,
+			animation_state: &app.animation_state,
+			elapsed_time: timed_loop_info.elapsed_time,
+			aspect_ratio,
+		};
+
+		context.fill_canvas_with_color(&game_texture_target, Color::rgb(255, 0, 0));
+
+		// draw elements
+		for element in &self.menu.elements {
+			element.draw(&app.window, &mut context, &app.cursor_position)
+		}
+
+		// draw cursor
+		context.draw_circle(&app.window, app.cursor_position, 0.01, Color::BLACK);
+		context.draw_circle(&app.window, app.cursor_position, 0.008, Color::WHITE);
+	}
+
+	fn get_runnable_change(&mut self) -> RunnableChange {
+		self.next_runnable_change
+	}
+}
