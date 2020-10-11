b8672308379c055d92e8dd2a9822aea718129853
memoryleak47 <memoryleak47@protonmail.ch>
2020-10-12 00:35:23 +0200

    implemented webgl jumping

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -6,8 +6,9 @@ impl InputDevice {
 	pub fn update(&mut self) -> InputState {
 		let [w, a, s, d] = get_wasd().into_serde::<[bool; 4]>().unwrap();
 		let x = d as i32 - a as i32;
+		let y = (w as i32 - s as i32) * 100;
 		InputState {
-			direction: GameVec::new(x,0),
+			direction: GameVec::new(x,y),
 			cursor: GameVec::new(0, 0),
 			just_up: w,
 			just_down: s,
