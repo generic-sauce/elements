002b69b79ae0dbf6cf19d477df8a0d7e8ad544de
memoryleak47 <memoryleak47@protonmail.ch>
2020-10-12 02:16:49 +0200

    webgl texturing [WIP]

--- web/site/draw.js
+++ web/site/draw.js
@@ -7,16 +7,24 @@ window.init_drawing = function() {
 	window.gl = canvas.getContext("webgl");
 
 	const vsSource = `
-		attribute vec4 aVertexPosition;
+		attribute vec2 aVertexPosition;
+		attribute vec2 aTextureCoord;
+
+		varying highp vec2 vTextureCoord;
 
 		void main() {
-			gl_Position = aVertexPosition;
+			gl_Position = vec4(aVertexPosition, 0, 0);
+			vTextureCoord = aTextureCoord;
 		}
 	`;
 
 	const fsSource = `
+		varying highp vec2 vTextureCoord;
+
+		uniform sampler2D uSampler;
+
 		void main() {
-			gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+			gl_FragColor = texture2D(uSampler, vTextureCoord);
 		}
 	`;
 
@@ -25,12 +33,29 @@ window.init_drawing = function() {
 		program: shaderProgram,
 		attribLocations: {
 			vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+			textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
 		},
 		uniformLocations: {
+			uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
 		},
 	};
 
-	window.buffers = { position: gl.createBuffer() };
+	const textureCoordBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+
+	const r = [
+		0.0,  0.0,
+		1.0,  0.0,
+		1.0,  1.0,
+		0.0,  1.0,
+	];
+	const tc = [r.slice(0, 6), r.slice(2,8), r.slice(0, 6), r.slice(2,8)].flat();
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tc), gl.STATIC_DRAW);
+	window.buffers = {
+		position: gl.createBuffer(),
+		textureCoord: textureCoordBuffer,
+	};
+	window.texture = loadTexture(gl, './file.png');
 }
 
 window.draw_world = function(world, constants) {
@@ -85,25 +110,45 @@ window.drawScene = function(gl, programInfo, buffers) {
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	{
-		const numComponents = 2;	// pull out 2 values per iteration
-		const type = gl.FLOAT;		// the data in the buffer is 32bit floats
-		const normalize = false;	// don't normalize
-		const stride = 0;				 // how many bytes to get from one set of values to the next
-															// 0 = use type and numComponents above
-		const offset = 0;				 // how many bytes inside the buffer to start from
-		gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-		gl.vertexAttribPointer(
-				programInfo.attribLocations.vertexPosition,
-				numComponents,
-				type,
-				normalize,
-				stride,
-				offset);
-		gl.enableVertexAttribArray(
-				programInfo.attribLocations.vertexPosition);
-	}
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+  }
+
+  {
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textureCoord);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.textureCoord,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.textureCoord);
+  }
 
 	gl.useProgram(programInfo.program);
+	gl.activeTexture(gl.TEXTURE0);
+	gl.bindTexture(gl.TEXTURE_2D, window.texture);
+	gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
 
 	{
 		const offset = 0;
@@ -158,4 +203,54 @@ function loadShader(gl, type, source) {
 	}
 
 	return shader;
+}
+
+function loadTexture(gl, url) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Because images have to be download over the internet
+  // they might take a moment until they are ready.
+  // Until then put a single pixel in the texture so we can
+  // use it immediately. When the image has finished downloading
+  // we'll update the texture with the contents of the image.
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const width = 1;
+  const height = 1;
+  const border = 0;
+  const srcFormat = gl.RGBA;
+  const srcType = gl.UNSIGNED_BYTE;
+  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                pixel);
+
+  const image = new Image();
+  image.onload = function() {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                  srcFormat, srcType, image);
+
+    // WebGL1 has different requirements for power of 2 images
+    // vs non power of 2 images so check if the image is a
+    // power of 2 in both dimensions.
+    if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+       // Yes, it's a power of 2. Generate mips.
+       gl.generateMipmap(gl.TEXTURE_2D);
+    } else {
+       // No, it's not a power of 2. Turn off mips and set
+       // wrapping to clamp to edge
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    }
+  };
+  image.src = url;
+
+  return texture;
+}
+
+function isPowerOf2(value) {
+  return (value & (value - 1)) == 0;
 }
