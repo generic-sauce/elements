cdf28e335e5d95ae0b1a4a41798de270832599fb
memoryleak47 <memoryleak47@protonmail.ch>
2020-10-13 23:54:37 +0200

    started with renderingr

---
+++ web/site/draw/math.js
@@ -0,0 +1,29 @@
+e2.max_game_point = function() {
+	return e2.world.tilemap.size
+		.map(x => x * 256);
+}
+
+e2.game_to_screen_point = function(p) {
+	const max = e2.max_game_point();
+	return [p[0] / max[0], p[1] / max[1]]
+		.map(x => 2*x - 1);
+}
+
+e2.player_rect = function(i) {
+	const lb = e2.world.players[i].left_bot;
+	const s = [2000, 2000]; // TODO window.constants.player_size;
+	const game_rect = [
+		[lb[0]       , lb[1]],
+		[lb[0] + s[0], lb[1]],
+		[lb[0]       , lb[1] + s[1]],
+		[lb[0] + s[0], lb[1] + s[1]],
+	];
+	let screen_rect = game_rect.map(e2.game_to_screen_point);
+	return [
+		screen_rect[0][0], screen_rect[0][1],
+		screen_rect[1][0], screen_rect[1][1],
+		screen_rect[2][0], screen_rect[2][1],
+		screen_rect[3][0], screen_rect[3][1],
+	];
+
+}
--- web/site/index.js
+++ web/site/index.js
@@ -1,16 +1,23 @@
-// TODO nice main-loop
-function schedule(w, js) {
+// the object storing the state of our application
+window.e2 = {};
 
-	js.tick_world_nohandler(w);
-	console.log(js.world_to_json(w).players[0].left_bot);
+import("./draw/mod.js");
 
-	const re_schedule = function() { schedule(w, js); }
-	setTimeout(re_schedule, 16);
+function schedule() {
+	e2.rust.tick_world_nohandler(e2.world_ptr);
+	e2.world = e2.rust.world_to_json(e2.world_ptr);
+	e2.draw_world();
+
+	setTimeout(schedule, 16);
 }
 
 const js = import("./node_modules/elements2/elements2.js");
-js.then(js => {
-	js.init();
-	const w = js.new_world();
-	schedule(w, js);
+js.then(rust => {
+	e2.rust = rust;
+
+	e2.rust.init();
+	e2.init_drawing();
+	e2.world_ptr = e2.rust.new_world();
+
+	schedule();
 });
