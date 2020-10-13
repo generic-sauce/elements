1404ad7fc20c6ac5b40f779fa62271d9bda4234b
memoryleak47 <memoryleak47@protonmail.ch>
2020-10-14 00:43:37 +0200

    <No commit-message>

--- web/site/src/main.js
+++ web/site/src/main.js
@@ -4,11 +4,22 @@ window.e2 = {};
 import("./draw/mod.js");
 import("./input.js");
 
-function schedule() {
+function init() {
+	e2.rust.init();
+	e2.cnst = e2.rust.constants();
+	e2.init_drawing();
+	e2.init_input();
+	e2.world_ptr = e2.rust.new_world();
+}
+
+function tick() {
 	e2.rust.tick_world(e2.world_ptr, e2.input_states);
 	e2.world = e2.rust.world_to_json(e2.world_ptr);
 	e2.draw_world();
+}
 
+function schedule() {
+	tick();
 	setTimeout(schedule, 16);
 }
 
@@ -16,11 +27,7 @@ const js = import("../node_modules/elements2/elements2.js");
 js.then(rust => {
 	e2.rust = rust;
 
-	e2.rust.init();
-	e2.cnst = e2.rust.constants();
-	e2.init_drawing();
-	e2.init_input();
-	e2.world_ptr = e2.rust.new_world();
+	init();
 
 	schedule();
 });
