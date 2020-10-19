11109c1b4682983bbe55abaceed1ab5838ed5b04
Bluemi <bruno.schilling@protonmail.ch>
2020-10-19 20:15:38 +0200

    warning if glslangvalidator is not installed

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::process::{Command, exit};
+use std::io::ErrorKind;
 
 fn compile_shader(shader_name: &str) {
 	let glsl_path = format!("res/shader/{}{}", shader_name, ".glsl");
@@ -12,13 +13,21 @@ fn compile_shader(shader_name: &str) {
 		.arg("-V")
 		.arg(glsl_path)
 		.arg("-o").arg(spv_path)
-		.status()
-		.unwrap();
+		.status();
 
-	match status.code() {
-		Some(code) => if code != 0 { exit(code); },
-		None => if !status.success() { exit(1); },
-	}
+    let status = match status.map_err(|e| e.kind()) {
+        Err(ErrorKind::NotFound) => {
+            println!("cargo:warning=glslangValidator not installed. Skipping shader checks.");
+            exit(0)
+        },
+        Err(e) => Err(e).unwrap(),
+        Ok(exit_status) => exit_status,
+    };
+
+    match status.code() {
+        Some(code) => if code != 0 { exit(code); },
+        None => if !status.success() { exit(1); },
+    }
 }
 
 fn main() {
