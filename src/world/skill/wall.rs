384ae4594f651563ceaf05e1d9cb7603cb086815
memoryleak47 <memoryleak47@protonmail.ch>
2020-10-26 15:43:05 +0100

    now walls can be refreshed

--- src/world/skill/wall.rs
+++ src/world/skill/wall.rs
@@ -19,9 +19,13 @@ impl World {
 
 	fn wall_from_to(&mut self, p: usize, from: GameVec, to: GameVec, handler: &mut impl EventHandler) {
 		let n = (from - to).length() / TILESIZE * 8; // is this well?
-		for i in 0..n {
-			let current = from * (n-i-1) / (n-1) + to * i / (n-1);
-			self.wall(p, current, handler);
+		if n == 0 {
+			self.wall(p, from, handler);
+		} else {
+			for i in 0..n {
+				let current = from * (n-i-1) / (n-1) + to * i / (n-1);
+				self.wall(p, current, handler);
+			}
 		}
 	}
 
@@ -29,12 +33,19 @@ impl World {
 		let pos_tile: TileVec = pos.into();
 		let tile = self.tilemap.get(pos_tile);
 
-		if tile != Tile::Void { return; }
+		let refill_amount = match tile {
+			Tile::Void => WALL_LIFETIME,
+			Tile::Wall { owner, remaining_lifetime } if owner == p => {
+				WALL_LIFETIME - remaining_lifetime
+			},
+			_ => return,
+		};
+
 		if (0..2).any(|i| self.players[i].collides_tile(pos_tile)) {
 			return;
 		}
 
-		if self.alloc_wall(p).is_none() {
+		if self.alloc_wall_lifetime(p, refill_amount).is_none() {
 			self.players[p].last_wall_pos = None;
 			return;
 		}
@@ -42,27 +53,26 @@ impl World {
 		self.players[p].last_wall_pos = Some(pos);
 		self.tilemap.set(pos_tile, Tile::Wall { owner: p, remaining_lifetime: WALL_LIFETIME });
 		handler.tilemap_changed();
-
 	}
 
-	fn alloc_wall(&mut self, p: usize) -> Option<()> {
+	fn alloc_wall_lifetime(&mut self, p: usize, amount: u32) -> Option<()> {
 		let mut pl = &mut self.players[p];
 
 		// allocate free_wall
-		if pl.free_wall == 0 {
+		if pl.free_wall_lifetime < amount {
 			for inner_v in self.fluidmap.grid.iter_mut() {
-				if inner_v.drain_filter(|x| x.owner == p).next().is_some() {
-					pl.free_wall += WALLS_PER_FLUID;
-					break;
+				for _ in  inner_v.drain_filter(|x| x.owner == p) {
+					pl.free_wall_lifetime += WALLS_PER_FLUID * WALL_LIFETIME;
+					if pl.free_wall_lifetime >= amount { break; }
 				}
 			}
 		}
 
-		if pl.free_wall == 0 {
+		if pl.free_wall_lifetime < amount {
 			return None;
 		}
 
-		pl.free_wall -= 1;
-		return Some(());
+		pl.free_wall_lifetime -= amount;
+		Some(())
 	}
 }
