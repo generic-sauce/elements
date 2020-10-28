f96b60312e0edc575248fa449bbea04d70625732
memoryleak47 <memoryleak47@protonmail.ch>
2020-10-28 02:02:41 +0100

    wall modules

--- src/world/skill/wall/pathfinder.rs
+++ src/world/skill/wall/pathfinder.rs
@@ -1,55 +1,13 @@
 use crate::prelude::*;
 
-const WALLS_PER_FLUID: u32 = 6;
-
 impl World {
-	pub(in super) fn handle_wall(&mut self, p: usize, handler: &mut impl EventHandler) {
-		let player = &mut self.players[p];
-		let cursor = player.cursor_position();
-		let from = match player.wall_mode {
-			WallMode::NoFluids => return,
-			WallMode::NotWalling => cursor,
-			WallMode::InProgress(x) => x,
-		};
-
-		player.wall_mode = WallMode::InProgress(cursor);
-
-		self.wall_from_to(p, from, cursor, handler);
-	}
-
-	pub(in super) fn stop_wall(&mut self, p: usize) {
-		self.players[p].wall_mode = WallMode::NotWalling;
-	}
-
-	fn wall_from_to(&mut self, p: usize, from: GameVec, to: GameVec, handler: &mut impl EventHandler) {
-		let path = self.generate_wall_path(p, from, to);
-		for t in path {
-			if self.wall(p, t, handler).is_none() {
-				self.players[p].wall_mode = WallMode::NoFluids;
-				return;
-			}
-		}
-	}
-
-	fn assert_unglitched_path(&self, path: &[TileVec]) {
-		assert!(path.iter().all(|t| !self.coll(*t)));
-		self.assert_path(path);
-	}
-
-	fn assert_path(&self, path: &[TileVec]) {
-		for i in 0..path.len()-1 {
-			let diff = path[i] - path[i+1];
-			assert_eq!(diff.x.abs() + diff.y.abs(), 1);
-		}
-	}
-
-	fn generate_wall_path(&self, p: usize, from: GameVec, to: GameVec) -> Vec<TileVec> {
+	pub(super) fn generate_wall_path(&self, p: usize, from: GameVec, to: GameVec) -> Vec<TileVec> {
 		let mut path = self.direct_path(p, from, to);
 
 		self.assert_path(&path[..]);
 
 		while let Some(i) = path.iter().position(|tile| self.coll(*tile)) {
-			let before_gap = i-1;
+			let before_gap = i - 1;
 			let after_gap = path.iter().enumerate().position(|(j, tile)| j > i && !self.coll(*tile)).unwrap();
 
 			self.assert_path(&path[..]);
@@ -92,79 +50,16 @@ impl World {
 		}
 
 		// remove diagonals
-		while let Some(i) = (0..direct_path.len() - 1).find(|&i| (direct_path[i] - direct_path[i+1]).length_squared() > 1) {
-			let new: TileVec = v(direct_path[i].x, direct_path[i+1].y);
+		while let Some(i) = (0..direct_path.len() - 1).find(|&i| (direct_path[i] - direct_path[i + 1]).length_squared() > 1) {
+			let new: TileVec = v(direct_path[i].x, direct_path[i + 1].y);
 
 			assert_eq!((new - direct_path[i]).length_squared(), 1);
-			assert_eq!((new - direct_path[i+1]).length_squared(), 1);
+			assert_eq!((new - direct_path[i + 1]).length_squared(), 1);
 
-			direct_path.insert(i+1, new);
+			direct_path.insert(i + 1, new);
 		}
 
 		direct_path
-	}
-
-	fn wall(&mut self, p: usize, pos_tile: TileVec, handler: &mut impl EventHandler) -> Option<()> {
-		assert!(!self.coll(pos_tile));
-
-		let tile = self.tilemap.get(pos_tile);
-
-		let refill_amount = match tile {
-			Tile::Void => WALL_LIFETIME,
-			Tile::Wall { owner, remaining_lifetime } if owner == p => {
-				WALL_LIFETIME - remaining_lifetime
-			},
-			_ => return Some(()),
-		};
-
-		self.alloc_wall_lifetime(p, refill_amount)?;
-
-		self.tilemap.set(pos_tile, Tile::Wall { owner: p, remaining_lifetime: WALL_LIFETIME });
-		handler.tilemap_changed();
-
-		Some(())
-	}
-
-	fn alloc_wall_lifetime(&mut self, p: usize, amount: u32) -> Option<()> {
-		let mut pl = &mut self.players[p];
-
-		// allocate free_wall
-		if pl.free_wall_lifetime < amount {
-			for inner_v in self.fluidmap.grid.iter_mut() {
-				while let Some(i) = inner_v.iter().position(|x| x.owner == p) {
-					inner_v.swap_remove(i);
-					pl.free_wall_lifetime += WALLS_PER_FLUID * WALL_LIFETIME;
-					if pl.free_wall_lifetime >= amount { break; }
-				}
-			}
-		}
-
-		if pl.free_wall_lifetime < amount {
-			return None;
-		}
-
-		pl.free_wall_lifetime -= amount;
-		Some(())
-	}
-
-	// return a position close to pos, not colliding with the player p
-	fn unglitch(&self, p: usize, start: GameVec) -> GameVec {
-		let pl = &self.players[p];
-		let center = pl.center_position();
-
-		let mut diff = start - center;
-
-		// in order to prevent division by zero
-		if diff == v(0, 0) {
-			diff = v(1, 0);
-		}
-
-		const STEP: i32 = 4;
-
-		// binary search would be much faster - or one could even do it correctly ^^'
-		(0..).map(|i| start + diff.with_length(i * STEP))
-			.find(|pos| !pl.collides_tile(pos.to_tile()))
-			.unwrap()
 	}
 
 	fn pathfind(&self, start: TileVec, gap_dir: Direction, goal: TileVec) -> Vec<TileVec> {
@@ -172,10 +67,6 @@ impl World {
 		let p2 = self.pathfind_impl(start, gap_dir, goal, false);
 
 		if p1.len() < p2.len() { p1 } else { p2 }
-	}
-
-	fn coll(&self, tile_pos: TileVec) -> bool {
-		self.players.iter().any(|pl| pl.collides_tile(tile_pos))
 	}
 
 	// TODO may infinite-loop if you have >= 4 players
@@ -212,6 +103,22 @@ impl World {
 		}
 
 		path
+	}
+
+	pub(super) fn coll(&self, tile_pos: TileVec) -> bool {
+		self.players.iter().any(|pl| pl.collides_tile(tile_pos))
+	}
+
+	fn assert_unglitched_path(&self, path: &[TileVec]) {
+		assert!(path.iter().all(|t| !self.coll(*t)));
+		self.assert_path(path);
+	}
+
+	fn assert_path(&self, path: &[TileVec]) {
+		for i in 0..path.len() - 1 {
+			let diff = path[i] - path[i + 1];
+			assert_eq!(diff.x.abs() + diff.y.abs(), 1);
+		}
 	}
 }
 
