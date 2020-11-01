8fbbe672dafbba107e975a4da8f28dd0d28e9da5
Bluemi <bruno.schilling@protonmail.ch>
2020-11-01 23:09:44 +0100

    splitted native/web graphics/input backends

--- src/backend/graphics/mod.rs
+++ src/backend/graphics/mod.rs
@@ -1,5 +1,11 @@
+use crate::prelude::*;
+
 #[cfg(feature = "web-client")] mod web;
 #[cfg(feature = "web-client")] pub use web::*;
 
 #[cfg(feature = "native-client")] mod native;
 #[cfg(feature = "native-client")] pub use native::*;
+
+pub trait GraphicsBackend {
+	fn draw(&mut self, draw: Draw);
+}
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -11,21 +11,9 @@ pub trait Backend {
 	type GraphicsBackend: GraphicsBackend;
 }
 
-pub trait InputBackend {
-	type EventIterator<'a>: Iterator<Item=PeripheralsUpdate>;
-
-	fn events(&mut self) -> Self::EventIterator<'_>;
-	fn gamepad(&mut self, gamepad_id: u32) -> RawGamepadState;
-	fn tick(&mut self);
-}
-
-pub trait GraphicsBackend {
-	fn draw(&mut self, draw: Draw);
-}
-
 pub struct NativeBackend;
 
 impl Backend for NativeBackend {
 	 type InputBackend = NativeInputBackend;
 	 type GraphicsBackend = NativeGraphicsBackend;
- }
+}
