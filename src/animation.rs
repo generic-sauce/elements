fa7ca19a19c8b4b334c0541db90d0a75365a6f35
Bluemi <bruno.schilling@protonmail.ch>
2020-11-06 21:01:30 +0100

    slower animations

--- src/animation.rs
+++ src/animation.rs
@@ -86,14 +86,14 @@ macro_rules! setup {
 }
 
 setup!(
-	BluePlayerIdle: "images/player_blue/player_idle", 5, 8,
-	BluePlayerRun: "images/player_blue/player_run", 6, 4,
-	BluePlayerJump: "images/player_blue/player_jump", 2, 4,
-	BluePlayerFall: "images/player_blue/player_fall", 2, 4,
-	BluePlayerFallSlow: "images/player_blue/player_fall_slow", 2, 4,
-	RedPlayerIdle: "images/player_red/player_idle", 5, 8,
-	RedPlayerRun: "images/player_red/player_run", 4, 4,
-	RedPlayerJump: "images/player_red/player_jump", 2, 4,
-	RedPlayerFall: "images/player_red/player_fall", 2, 4,
-	RedPlayerFallSlow: "images/player_red/player_fall_slow", 2, 4
+	BluePlayerIdle: "images/player_blue/player_idle", 5, 9,
+	BluePlayerRun: "images/player_blue/player_run", 6, 6,
+	BluePlayerJump: "images/player_blue/player_jump", 2, 6,
+	BluePlayerFall: "images/player_blue/player_fall", 2, 6,
+	BluePlayerFallSlow: "images/player_blue/player_fall_slow", 2, 6,
+	RedPlayerIdle: "images/player_red/player_idle", 5, 9,
+	RedPlayerRun: "images/player_red/player_run", 4, 6,
+	RedPlayerJump: "images/player_red/player_jump", 2, 6,
+	RedPlayerFall: "images/player_red/player_fall", 2, 6,
+	RedPlayerFallSlow: "images/player_red/player_fall_slow", 2, 6
 );
