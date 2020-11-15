e123d4a2ee7ec7ed031fefeebb191f2e1d51c8b3
Bluemi <bruno.schilling@protonmail.ch>
2020-11-15 17:54:18 +0100

    fix and removed unused

--- rocket-server/src/main.rs
+++ rocket-server/src/main.rs
@@ -5,15 +5,9 @@
 use serde::{Serialize, Deserialize};
 use std::process::Command;
 use std::str;
-use rocket::request::{FromRequest, Outcome, Request};
-use rocket::http::Status;
 use rocket_contrib::json::Json;
 
 const ELEMENTS_DEPLOY_DIRECTORY: &str = "/home/sauce/elements_deploy";
-
-pub enum GithubEvent {
-	Push
-}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GithubPushCommit {
@@ -23,24 +17,6 @@ pub struct GithubPushCommit {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GithubPushHook {
 	pub commits: Vec<GithubPushCommit>,
-}
-
-impl<'r, 'a> FromRequest<'r, 'a> for GithubEvent {
-	type Error = ();
-
-	fn from_request(request: &'r Request<'a>) -> Outcome<Self, ()> {
-		let keys = request.headers().get("X-Github-Event").collect::<Vec<_>>();
-		if keys.len() != 1 {
-			return Outcome::Failure((Status::BadRequest, ()));
-		}
-
-		let event = match keys[0] {
-			"push" => GithubEvent::Push {},
-			_ => { return Outcome::Failure((Status::BadRequest, ())) },
-		};
-
-		Outcome::Success(event)
-	}
 }
 
 #[get("/")]
@@ -54,7 +30,7 @@ fn deploy(event: Json<GithubPushHook>) {
 	let deploy_commit = &event.commits.iter().any(|c| c.message.contains("#deploy"));
 
 	// TODO: do not call bash script, but use rust bindings
-	if deploy_commit {
+	if *deploy_commit {
 		match Command::new("bash").arg("-c").arg("./deploy.sh").current_dir(ELEMENTS_DEPLOY_DIRECTORY).output() {
 			Ok(x) => {
 				println!("Deployed.status: {}", x.status);
