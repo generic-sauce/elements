5310783a92baebb0c714f96b271feede3de22171
Bluemi <bruno.schilling@protonmail.ch>
2020-11-15 15:30:08 +0100

    added rocket-server

---
+++ rocket-server/src/main.rs
@@ -0,0 +1,24 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use std::process::Command;
+
+const ELEMENTS_DEPLOY_DIRECTORY: &str = "/home/sauce/elements_deploy";
+
+#[get("/")]
+fn index() -> &'static str {
+    "This is the elements frontpage. Have fun :3. Go to /elements/game for the game."
+}
+
+#[post("/deploy")]
+fn deploy() {
+	match Command::new("bash").arg("-c").arg("deploy.sh").current_dir(ELEMENTS_DEPLOY_DIRECTORY).output() {
+		Ok(_) => {}
+		Err(e) => { println!("Error executing deploy.sh: {}", e) }
+	}
+}
+
+fn main() {
+    rocket::ignite().mount("/elements", routes![index, deploy]).launch();
+}
