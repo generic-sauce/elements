f0e89e5b29272dd0edfe5be0a323baf4d2ac9abc
ichinii <ogtoad@protonmail.com>
2020-12-01 03:17:34 +0100

    new tilemap look

--- res/shader/tilemap.frag.glsl
+++ res/shader/tilemap.frag.glsl
@@ -8,7 +8,7 @@ layout (set = 0, binding = 1) uniform sampler tilemap_sam;
 layout (location = 0) in vec2 uv;
 
 float n21(vec2 s) {
-	return fract(9542.276 * sin(dot(vec2(527.831, 699.258), s)));
+	return fract(9542.276 * sin(dot(vec2(527.831, 699.258), s + vec2(1.753, 1.245))));
 }
 
 float smooth_n21(vec2 seed) {
@@ -34,32 +34,44 @@ float round_n21(vec2 seed) {
 	return n0 * .1 + n1 * .2 + n2 * .3 + n3 * .4;
 }
 
+vec2 size() {
+	return textureSize(sampler2D(tilemap_tex, tilemap_sam), 0);
+}
+
+int tile(vec2 uv) {
+	return int(255.9 * texture(sampler2D(tilemap_tex, tilemap_sam), uv).r);
+}
+
 vec3 ground_color(vec2 uv) {
-	const int count = 5;
-	const vec3 colors[] = vec3[count] (
-			vec3(20,36,21),
-			vec3(21,29,15),
-			vec3(24,43,29),
-			vec3(35,49,27),
-			vec3(28,48,27)
-	);
+	vec2 s = size();
+	float a = s.x / s.y;
+	vec2 px = 1. / s;
+	float up0 = tile(uv + vec2(0, px.y * 1.)) == 1 ? 1. : 0.;
+	float up1 = tile(uv + vec2(0, px.y * 2.)) == 1 ? 1. : 0.;
+	float up2 = tile(uv + vec2(0, px.y * 3.)) == 1 ? 1. : 0.;
+	float up3 = tile(uv + vec2(0, px.y * 4.)) == 1 ? 1. : 0.;
+	float up = (up0 + up1 + up2 + up3) / 4.;
 
-	vec2 tilemap_tex_size = textureSize(sampler2D(tilemap_tex, tilemap_sam), 0);
+	vec3 dirt0 = vec3(75, 50, 27) / 255. * 1.1;
+	vec3 dirt1 = vec3(229, 187, 128) / 255. * .7;
 
-	float h = round_n21(uv * tilemap_tex_size / 16.);
-	h = sign(h - .5) * pow(abs(h * 2. - 1.), .8) * .5 + .5;
-	int i = int(count * h);
-	vec3 c = colors[i];
+	vec3 grass0 = vec3(15, 75, 32) / 255.;
+	vec3 grass1 = vec3(123, 231, 118) / 255.;
 
-	return c / 255.;
+	float n = round_n21(floor(uv / px) * px * 2. * vec2(a, 1));
+	vec3 dirt = mix(dirt0, dirt1, vec3(floor(n * 4.) / 4.));
+	vec3 grass = mix(grass0, grass1, vec3(floor(n * 10.) / 10.));
+	vec3 c = mix(grass, dirt, step(.9, up + (n - .5)));
+
+	return c;
 }
 
 void main() {
 	vec3 c;
 	vec3 wall_color = vec3(.85, .95, .99);
 
-	int tile = int(255.9 * texture(sampler2D(tilemap_tex, tilemap_sam), uv).r);
-	switch (tile) {
+	int t = tile(uv);
+	switch (t) {
 	case 1: // ground
 		c = ground_color(uv);
 		break;
