055b831d6cfd25c81cc3a3d36b5df38b174f667a
memoryleak47 <memoryleak47@protonmail.ch>
2020-12-01 12:43:17 +0100

    ~audio.js

--- web/site/src/audio.js
+++ web/site/src/audio.js
@@ -1,3 +1,15 @@
 export function handle_command(cmd) {
-	console.log(cmd);
+	if ("QueueMusic" in cmd) {
+		queue_music(cmd["QueueMusic"])
+	} else if ("PlaySound" in cmd) {
+		play_sound(...cmd["PlaySound"])
+	}
+}
+
+function queue_music(file) {
+	console.log("TODO: queue music!");
+}
+
+function play_sound(file, volume) {
+	console.log("TODO: play sound " + file + " " + volume);
 }
