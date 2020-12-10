743d47f7ecda7ca445b9dee0c4dce9e6b31906fd
memoryleak47 <memoryleak47@protonmail.ch>
2020-12-10 16:04:40 +0100

    added old rand::random() instead of deterministic seeded random for debugging purposes

--- src/rng.rs
+++ src/rng.rs
@@ -1,13 +1,4 @@
-pub fn rng(rate: u32, antirate: u32, seed: [u32; 4]) -> bool {
-	let result = mini_rng(mini_rng(seed[0], seed[1]), mini_rng(seed[2], seed[3]));
+pub fn rng(rate: u32, antirate: u32, _seed: [u32; 4]) -> bool {
+	let result = rand::random::<u32>();
 	result % (rate + antirate) < rate
-}
-
-fn mini_rng(a: u32, b: u32) -> u32 {
-	use rand::{SeedableRng, RngCore};
-	use rand_xorshift::XorShiftRng;
-
-	let combined = (a as u64) + ((b as u64) << 32);
-
-	XorShiftRng::seed_from_u64(combined).next_u32()
 }
