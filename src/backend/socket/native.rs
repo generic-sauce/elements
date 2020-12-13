d1b7915feb057222da56d9f5113688062000542a
Bluemi <bruno.schilling@protonmail.ch>
2020-12-13 14:29:50 +0100

    lil improvement

--- src/backend/socket/native.rs
+++ src/backend/socket/native.rs
@@ -19,8 +19,7 @@ impl SocketBackend for NativeSocketBackend {
 	fn is_open(&self) -> bool { true }
 
 	fn send(&mut self, packet: &impl Packet) -> std::io::Result<()> {
-		send_packet(&mut self.0, packet)?;
-		Ok(())
+		send_packet(&mut self.0, packet)
 	}
 
 	fn try_recv<P: Packet>(&mut self) -> Option<P> {
