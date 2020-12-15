d3e95318e9d3a0d5b95a8063853cb3ca9edaad8b
ichinii <ogtoad@protonmail.com>
2020-12-15 21:29:08 +0100

    tilemap shader to web

--- res/web_shader/tilemap.frag.glsl.js
+++ res/web_shader/tilemap.frag.glsl.js
@@ -25,6 +25,7 @@ float smooth_n21(vec2 seed) {
 }
 
 float round_n21(vec2 seed) {
+	seed += 17.63;
 	float n0 = smooth_n21(seed);
 	float n1 = smooth_n21(seed * 2.);
 	float n2 = smooth_n21(seed * 4.);
@@ -33,43 +34,107 @@ float round_n21(vec2 seed) {
 	return n0 * .1 + n1 * .2 + n2 * .3 + n3 * .4;
 }
 
-vec3 color_by_index(int index) {
-	if (index == 0)
-		return vec3(20,36,21);
-	if (index == 1)
-		return vec3(21,29,15);
-	if (index == 2)
-		return vec3(24,43,29);
-	if (index == 3)
-		return vec3(35,49,27);
-	return vec3(28,48,27);
+vec2 tilemap_size() {
+	return tilemap_tex_size;
 }
 
-vec3 ground_color(vec2 uv) {
-	const int count = 5;
+int tile(vec2 uv) {
+	return int(255.9 * texture2D(tilemap_tex, uv).r);
+}
 
-	float h = round_n21(uv * tilemap_tex_size / 16.);
-	h = sign(h - .5) * pow(abs(h * 2. - 1.), .8) * .5 + .5;
-	int i = int(float(count) * h);
-	vec3 c = color_by_index(i);
+float tilef(vec2 uv) {
+	return 255. * texture2D(tilemap_tex, uv).r;
+}
 
-	return c / 255.;
+float ground(vec2 uv) {
+	return tile(uv) == 1 ? 1. : 0.;
+}
+
+float xor(float a, float b) {
+	return 1. - abs(1. - a - b);
+}
+
+vec4 ground_color(vec2 uv) {
+	// tweaks
+	const float density = 2.;
+	const float grass_scale = .7;
+	const int igrass_thickness = 3;
+	const float grass_thickness = float(igrass_thickness);
+	const float dirt_grass_threshold = .2;
+
+	// colors
+	vec3 dirt0 = vec3(75, 50, 27) / 255. * 1.1;
+	vec3 dirt1 = vec3(229, 187, 128) / 255. * .7;
+	vec3 grass0 = vec3(123, 231, 118) / 255. * .8;
+	vec3 grass1 = vec3(15, 75, 32) / 255.;
+
+	// consts
+	vec2 size = tilemap_size();
+	vec2 aspect = vec2(size.x / size.y, 1);
+	vec2 px = 1. / size;
+	vec2 pxx = vec2(px.x, 0);
+	vec2 pxy = vec2(0, px.y);
+
+	// grid cell local uv
+	vec2 lv = fract(uv * size);
+
+	float sum = 0.;
+	float all = 1.;
+	for (int i = 1; i < igrass_thickness + 1; ++i) {
+		float t = ground(uv + pxy * float(i));
+		all *= t;
+		sum += all;
+	}
+	float any = step(.001, sum);
+
+	float grass_portion = 0.;
+	grass_portion += lv.y + (grass_thickness - sum);
+	grass_portion += (1. - lv.x) * (1. - ground(uv + pxy * sum - pxx));
+	grass_portion += lv.x * (1. - ground(uv + pxy * sum + pxx));
+	grass_portion *= (grass_scale / grass_thickness);
+	grass_portion *= 1. - all;
+
+	float n_dirt = round_n21(uv * aspect * density);
+	float n_grass = mix(n_dirt, .0, grass_portion);
+
+	float dirt_color_ratio = floor(n_dirt * 4.) / 4.;
+	float grass_color_ratio = floor(n_grass / dirt_grass_threshold * 4.) / 4.;
+
+	vec3 dirt_color = mix(dirt0, dirt1, vec3(dirt_color_ratio));
+	vec3 grass_color = mix(grass0, grass1, vec3(grass_color_ratio));
+
+	vec3 c = mix(grass_color, dirt_color, step(dirt_grass_threshold, n_grass));
+
+	float left = ground(uv - pxx);
+	float right = ground(uv + pxx);
+	float a = 1.;
+	a *= (1. - any);
+	a *= xor(left, right);
+	a -= left * step(0., (1. - lv.y) - lv.x);
+	a -= right * step(0., (1. - lv.y) - (1. - lv.x));
+	a = 1. - a;
+
+	return vec4(c, a);
 }
 
 void main() {
-	vec3 c;
+	vec4 c;
 	vec3 wall_color = vec3(.85, .95, .99);
 
-	int tile = int(255.9 * texture2D(tilemap_tex, uv).r);
-	if (tile == 1) // ground
-		c = ground_color(uv);
-	else if (tile == 2) // wall player 0
-		c = wall_color;
-	else if (tile == 3) // wall player 1
-		c = (1. - wall_color) * .5;
-	else // background
+	int t = tile(uv);
+	if (t == 1) { // ground
+		c = ground_color(uv + .00001);
+	} else if (t == 2) { // wall player 0
+		c = vec4(wall_color, 1);
+	} else if (t == 3) { // wall player 1
+		c = vec4((1. - wall_color) * .5, 1);
+	} else {
+		c.a = 0.;
+	}
+
+	if (c.a < .5)
 		discard;
 
-	gl_FragColor = vec4(c, 1);
+	gl_FragColor = vec4(c.rgb, 1);
 }
 `
