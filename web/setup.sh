bae2e51c2f20636180a0fbb931b17af2f9bcf33d
memoryleak47 <memoryleak47@protonmail.ch>
2020-12-19 12:33:17 +0100

    fixed web

--- web/setup.sh
+++ web/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-(cd ..; wasm-pack build -d web/pkg -- --no-default-features --features "web-client") && (cd pkg; sudo npm link) && (cd site; npm link elements && npm install) && ln -s "$(readlink -f ../res)" site/res
+(cd ../web_client; wasm-pack build -d ../web/pkg) && (cd pkg; sudo npm link) && (cd site; npm link elements && npm install) && ln -s "$(readlink -f ../res)" site/res
