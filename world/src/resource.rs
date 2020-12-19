c11ac5befeabd3cc947b8b969400bfacf27abc1f
memoryleak47 <memoryleak47@protonmail.ch>
2020-12-19 13:04:10 +0100

    bugfix #deploy

--- world/src/resource.rs
+++ world/src/resource.rs
@@ -1,6 +1,20 @@
 use std::path::PathBuf;
 
+#[cfg(not(target_arch = "wasm32"))]
 pub fn res(s: &str) -> String {
+	fn res_dir() -> PathBuf {
+		let s = std::env::args()
+			.next()
+			.unwrap();
+
+		let mut p = PathBuf::from(s);
+		p.pop();
+		p.pop();
+		p.pop();
+		p.push("res");
+		p
+	}
+
 	let mut p = res_dir();
 	p.push(s);
 	p.to_str()
@@ -8,21 +22,7 @@ pub fn res(s: &str) -> String {
 		.to_string()
 }
 
-fn res_dir() -> PathBuf {
-	let s = std::env::args()
-		.next()
-		.unwrap();
-
-	let mut p = PathBuf::from(s);
-	p.pop();
-	p.pop();
-	p.pop();
-	p.push("res");
-	p
-}
-
-/*
+#[cfg(target_arch = "wasm32")]
 pub fn res(s: &str) -> String {
 	format!("res/{}", s)
 }
- */
