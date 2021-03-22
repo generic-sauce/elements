b3dd5df6093a1ad8eb003f59c340cc498e4e3077
Bluemi <bruno.schilling@protonmail.ch>
2021-03-22 23:41:49 +0100

    removed deploy hook from rocket server

--- infrastructure/rocket-server/src/main.rs
+++ infrastructure/rocket-server/src/main.rs
@@ -4,7 +4,6 @@
 
 use rocket::config::*;
 use rocket::response::Redirect;
-use serde::{Serialize, Deserialize};
 use std::process::{Command, Stdio};
 use std::{str, thread};
 use rocket_contrib::json::Json;
@@ -13,51 +12,7 @@ use rocket::State;
 use std::sync::{Arc, Mutex};
 use rocket_contrib::serve::StaticFiles;
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GithubPushCommit {
-	pub message: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GithubPushHook {
-	pub commits: Vec<GithubPushCommit>,
-}
-
-#[post("/", data = "<event>")]
-fn deploy(sender: State<Arc<Mutex<Sender<()>>>>, event: Json<GithubPushHook>) {
-	// parse event
-	let deploy_commit = event.commits.iter().any(|c| c.message.contains("#deploy"));
-
-	// TODO: do not call bash script, but use rust bindings
-	if deploy_commit {
-		sender.lock()
-			.unwrap()
-			.send(())
-			.unwrap();
-	}
-}
-
 fn main() {
-	let (sender, receiver) = channel::<()>();
-
-	thread::spawn(move || {
-		loop {
-			receiver.recv().unwrap();
-
-			println!("starting new deploy");
-			let mut command = Command::new("bash");
-			command.arg("-c").arg("./deploy.sh")
-				.current_dir("../deploy")
-				.stdout(Stdio::inherit())
-				.stderr(Stdio::inherit());
-
-			match command.output() {
-				Ok(_) => println!("Successfully executed deploy.sh"),
-				Err(e) => println!("Error executing deploy.sh: {}", e),
-			}
-		}
-	});
-
 	// redirect HTTP -> HTTPS
 	thread::spawn(|| {
 		let config = Config::build(Environment::Production)
@@ -76,8 +31,6 @@ fn main() {
 	});
 
     rocket::ignite()
-		.manage(Arc::new(Mutex::new(sender)))
-		.mount("/deploy", routes![deploy])
 		.mount("/", StaticFiles::from("./static"))
 		.launch();
 }
