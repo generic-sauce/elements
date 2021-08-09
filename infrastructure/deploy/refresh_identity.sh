a1977f71cef2298e5170cc1f035fabe03c61fae4
Bluemi <bruno.schilling@protonmail.ch>
2021-08-09 14:42:22 +0200

    improved refresh identity script

--- infrastructure/deploy/refresh_identity.sh
+++ infrastructure/deploy/refresh_identity.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# openssl pkcs12 -export -out identity.pfx -inkey /etc/letsencrypt/archive/generic-sauce.de/privkey2.pem -in /etc/letsencrypt/archive/generic-sauce.de/cert2.pem -certfile /etc/letsencrypt/archive/generic-sauce.de/chain2.pem
 openssl pkcs12 -export -out identity.pfx -inkey /etc/letsencrypt/live/generic-sauce.de/privkey.pem -in /etc/letsencrypt/live/generic-sauce.de/cert.pem -certfile /etc/letsencrypt/live/generic-sauce.de/chain.pem
+
+cp identity.pfx ./game_server/identity.pfx
+cp identity.pfx ./master_server/identity.pfx
