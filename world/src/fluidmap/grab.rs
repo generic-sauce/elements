47dc68b653f1606b62794ab203cceb07c2673bc3
memoryleak47 <memoryleak47@protonmail.ch>
2021-08-12 00:32:15 +0200

    bugfix

--- world/src/fluidmap/grab.rs
+++ world/src/fluidmap/grab.rs
@@ -7,16 +7,16 @@ impl FluidMap {
 	pub(in super) fn apply_grab(&self, mut f: Fluid, teams: &[u8], players: &[Player]) -> Fluid {
 		for (i, player) in players.iter().enumerate() {
 			if player.grab_cooldown.is_some() { continue; }
+			if f.team != teams[i] { continue; }
 
 			let cursor = player.cursor_position();
-
 			let condition = (cursor - f.position).as_short_as(CURSOR_GRAB_DIST) ||
 				self.neighbours(&f)
 					.any(|n|
-						f.team == teams[i]
-						&& (f.position - n.position).as_short_as(FLUID_GRAB_DIST)
+						(f.position - n.position).as_short_as(FLUID_GRAB_DIST)
 							&& n.state == FluidState::AtHand(i as u8)
 					);
+
 			if condition {
 				f.state = FluidState::AtHand(i as u8);
 				break;
