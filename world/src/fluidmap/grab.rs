5eb81637c056692fb21f83ab66b93bc05bb65922
memoryleak47 <memoryleak47@protonmail.ch>
2021-08-12 00:14:26 +0200

    fix in apply_grab

--- world/src/fluidmap/grab.rs
+++ world/src/fluidmap/grab.rs
@@ -4,7 +4,7 @@ const FLUID_GRAB_DIST: i32 = DESIRED_FLUID_DIST * 3 / 2;
 const CURSOR_GRAB_DIST: i32 = TILESIZE * 4;
 
 impl FluidMap {
-	pub(in super) fn apply_grab(&self, mut f: Fluid, players: &[Player]) -> Fluid {
+	pub(in super) fn apply_grab(&self, mut f: Fluid, teams: &[u8], players: &[Player]) -> Fluid {
 		for (i, player) in players.iter().enumerate() {
 			if player.grab_cooldown.is_some() { continue; }
 
@@ -13,7 +13,7 @@ impl FluidMap {
 			let condition = (cursor - f.position).as_short_as(CURSOR_GRAB_DIST) ||
 				self.neighbours(&f)
 					.any(|n|
-						f.team == n.team
+						f.team == teams[i]
 						&& (f.position - n.position).as_short_as(FLUID_GRAB_DIST)
 							&& n.state == FluidState::AtHand(i as u8)
 					);
