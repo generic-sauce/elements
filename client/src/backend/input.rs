f0868856419fc7dd2108520979ded9bb2aed1851
Bluemi <bruno.schilling@protonmail.ch>
2021-11-08 02:33:13 +0100

    fix

--- client/src/backend/input.rs
+++ client/src/backend/input.rs
@@ -1,7 +1,7 @@
 use crate::prelude::*;
 
 pub trait InputBackend {
-	type EventIterator<'a>: Iterator<Item=PeripheralsUpdate>;
+	type EventIterator<'a>: Iterator<Item=PeripheralsUpdate> where Self: 'a;
 
 	fn events(&mut self) -> Self::EventIterator<'_>;
 	fn gamepad(&mut self, gamepad_id: u32) -> RawGamepadState;
